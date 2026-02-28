a0858f73550caf9583fdd6731b94df0255096fd1
Amnaali1234 <164417454+Amnaali1234@users.noreply.github.com>
2026-02-28 23:56:43 +0500

    Add files via upload

---
+++ Assignment_03/app.js
@@ -0,0 +1,13 @@
+let age = 15;
+let visitSite = 14;
+let birthYear = 2006;
+let message = "Data type of my declared variable is number";
+let visitorsName = "John Doe";
+let productTitle = "T-shirt(s)";
+let quantity = 5;
+let storeName = "XYZ Clothing store";
+alert(`I am ${age} years old`);
+alert(`You have visited this site ${visitSite} times`);
+document.writeln(`My birth year is ${birthYear}<br>`);
+document.writeln(`${message}<br>`);
+document.writeln(`<b>${visitorsName}</b> ordered <b>${quantity}</b> <b>${productTitle}</b> on ${storeName}`)
